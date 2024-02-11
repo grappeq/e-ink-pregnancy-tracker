d4dccf518fc38cffcd8a50e0268a2d161ea4b3d6
Kacper Grabowski <kacper@grabow.ski>
2024-02-11 18:17:26 +0100

    fix local test

--- local_test.py
+++ local_test.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import logging
-import time
+from datetime import datetime, timedelta
 
 from pregnancy_tracker import ScreenUI, Pregnancy
 
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    pregnancy = Pregnancy(time.time()+3600*24*100)
+    birth_time = datetime.now() + timedelta(days=100)
+    pregnancy = Pregnancy(birth_time.strftime("%Y-%m-%d"))
     screen_ui = ScreenUI(264, 176, pregnancy)
     himage = screen_ui.draw()
     himage.show()
